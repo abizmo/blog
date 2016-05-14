9f6c194b35fe6194b97730fb51f2dcdcc2dd042e
abizmo <abizmo@gmail.com>
2016-05-14 08:24:45 +0100

    Adding edit & delete button to a post

--- client/app/posts/posts.controller.js
+++ client/app/posts/posts.controller.js
@@ -6,8 +6,9 @@ angular.module('blogApp')
       $scope.posts = response.data;
     });
   }])
-  .controller('DetailCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+  .controller('DetailCtrl', ['$scope', '$http', '$routeParams', 'Auth', function ($scope, $http, $routeParams, Auth) {
     $scope.post = '';
+    $scope.isLoggedIn = Auth.isLoggedIn;
     $http.get('/api/posts/' + $routeParams.id).then(response => {
       $scope.post = response.data;
     });
