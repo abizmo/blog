511e92e58211c9235e379f265cf8957a14c36b67
abizmo <abizmo@gmail.com>
2016-05-15 23:34:00 +0100

    Adding posts

---
+++ client/app/posts/new/new.controller.js
@@ -0,0 +1,44 @@
+'use strict';
+
+class NewCtrl {
+  //start-non-standard
+  post = {};
+  errors = {};
+  submitted = false;
+  //end-non-standard
+
+  constructor($http, Auth, $location) {
+    this.$http = $http;
+    this.Auth = Auth;
+    this.$location = $location;
+  }
+
+  addPost(form) {
+    this.submitted = true;
+
+    if (form.$valid) {
+      this.$http.post('/api/posts/', {
+        title: this.post.title,
+        body: this.post.body,
+        author: this.Auth.getCurrentUser()._id
+      })
+        .then(response => {
+          // Post created, redirect to posts/:id
+          this.$location.path('/posts');
+        })
+        .catch(err => {
+          err = err.data;
+          this.errors = {};
+
+          // Update validity of form fields that match the mongoose errors
+          angular.forEach(err.errors, (error, field) => {
+            form[field].$setValidity('mongoose', false);
+            this.errors[field] = error.message;
+          });
+        });
+    }
+  }
+}
+
+angular.module('blogApp')
+  .controller('NewCtrl', NewCtrl);
--- client/app/posts/posts.js
+++ client/app/posts/posts.js
@@ -7,6 +7,11 @@ angular.module('blogApp')
         templateUrl: 'app/posts/posts.html',
         controller: 'ListCtrl'
       })
+      .when('/posts/new', {
+        templateUrl: 'app/posts/new/new.html',
+        controller: 'NewCtrl',
+        controllerAs: 'vm'
+      })
       .when('/posts/:id', {
         templateUrl: 'app/posts/detail.html',
         controller: 'DetailCtrl'
